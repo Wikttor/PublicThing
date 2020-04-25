825f4b44fecf3f60a8faa929f605faafdaa26fb4
Wiktor Maternik <wiktormaternik@gmail.com>
2020-04-25 20:36:51 +0200

    now really moving those scripts

---
+++ Assets/Scripts/NearestNavPointTracker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NearestNavPointTracker : MonoBehaviour
+{
+    public ITrackableNearestNavPoint i_trackedObject = null;
+    public Enemy i_myEnemy;
+    public bool i_trackPlayer = false;
+    public bool i_trackEnemy = false;
+
+    private void Start()
+    {
+
+        if (i_trackEnemy)
+        {
+            i_trackedObject = i_myEnemy;
+        }
+    }
+    void Update()
+    {
+        if (i_trackedObject == null && i_trackPlayer)
+        {
+            i_trackedObject = PlayerMovementAndRotation.staticRef;
+        }
+        if ( i_trackedObject != null)
+        {
+            this.transform.position = i_trackedObject.NearestNavPointPositionGet();
+        }
+    }
+}
+
+
+public interface ITrackableNearestNavPoint
+{
+    Vector3 NearestNavPointPositionGet();
+}
